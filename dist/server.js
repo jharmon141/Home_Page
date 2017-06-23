bf13d3d0fc5feffe8e3de7b416f3dc645b33af4c
jharmon141 <jeremy.l.harmon@gmail.com>
2017-06-22 21:26:05 -0500

    :robot:

--- dist/server.js
+++ dist/server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname));
+var history = require('connect-history-api-fallback');
 var port = process.env.PORT || 5000;
 
+app = express();
+app.use(history());
+app.use(serveStatic(__dirname));
 
 
 app.listen(port);
